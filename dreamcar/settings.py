1c1bd4aa909a6e4e26bd1a224d2763153f8ea71c
abee84 <78616174+abee84@users.noreply.github.com>
2022-12-02 12:51:32 +0530

    debug

--- dreamcar/settings.py
+++ dreamcar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=False)
+DEBUG = config('DEBUG', cast=bool, default=True)
 
 ALLOWED_HOSTS = ['*']
 
